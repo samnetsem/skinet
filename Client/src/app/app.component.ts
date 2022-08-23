3b49fbbf870fd6cccf091061a8c28a13f112a120
Samuel Mulugeta <samnetsem@gmail.com>
2022-08-23 12:25:36 +0300

    End of Section 9

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
+import { IProduct } from './shared/models/product';
+import { IPagination } from './shared/models/pagination';
 
 
 @Component({
@@ -11,21 +11,12 @@ import { IPagination } from './models/pagination';
 })
 export class AppComponent implements OnInit  {
   title = 'Skinet';
-  products:IProduct[]=[];
-  constructor(private http:HttpClient)
+
+  constructor()
   {
 
   }
   ngOnInit(): void {
-  this.http.get('https://localhost:7056/api/products?pageSize=50').
-  subscribe(
-    (response:any)=>{
-      this.products=response.data;
-    },
-    error=>{
-      console.log(error);
-    }
 
-  );
 }
 }
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
